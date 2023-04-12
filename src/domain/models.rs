c4d5a50630a75e99deb78cfcbd06bf82fd4a0b20
sawaca96 <sawaca96@gmail.com>
2023-04-13 00:21:39 +0900

    ♻️ add models method

--- src/domain/models.rs
+++ src/domain/models.rs
@@ -8,7 +8,7 @@ pub mod user {
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     pub struct User {
         pub id: Uuid,
-        pub name: String,
+        pub nickname: String,
         pub email: String,
     }
 }
@@ -19,30 +19,17 @@ pub mod account {
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     pub struct Account {
         pub id: Uuid,
-        pub user_id: Uuid,
+        pub user: user::User,
         pub cash: Decimal,
     }
 
     impl Account {
-        pub fn execute_trade(&mut self, trade: &trade::Trade) -> Result<(), &'static str> {
-            // Check if the user has sufficient funds
-            if self.cash < trade.total_cost() {
-                return Err("Insufficient funds");
-            }
+        pub fn has_sufficient_cash(&self, required_cash: Decimal) -> bool {
+            self.cash >= required_cash
+        }
 
-            // Lock the account instance to prevent race conditions
-            // ...
-
-            // Execute the trade
-            // ...
-
-            // Update the account's cash_balance
-            self.cash -= trade.total_cost();
-
-            // Unlock the account instance
-            // ...
-
-            Ok(())
+        pub fn reserve_cash(&mut self, amount: Decimal) {
+            self.cash -= amount;
         }
     }
 }
@@ -64,11 +51,31 @@ pub mod position {
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     pub struct Position {
         pub account_id: Uuid,
-        pub crypto_id: u64,
+        pub crypto: crypto::Crypto,
         pub quantity: Decimal,
+        pub sellable_quantity: Decimal,
         pub buy_price: Decimal,
         pub purchase: Decimal,
         pub created_at: u64,
+    }
+
+    impl Position {
+        pub fn can_sell(&self, quantity: Decimal) -> bool {
+            self.sellable_quantity >= quantity
+        }
+
+        pub fn reserve_for_sell(&mut self, amount: Decimal) {
+            self.sellable_quantity -= amount;
+        }
+
+        pub fn update_position(&mut self, trade: &trade::Trade) {
+            let new_quantity = self.quantity + trade.quantity;
+            let new_purchase = self.purchase + trade.total_cost();
+            self.buy_price = new_purchase / new_quantity;
+            self.purchase = new_purchase;
+            self.quantity = new_quantity;
+            self.sellable_quantity += trade.quantity;
+        }
     }
 }
 
@@ -76,10 +83,16 @@ pub mod trade {
     use super::*;
 
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    pub enum TradeType {
+        Buy,
+        Sell,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     pub struct Trade {
         pub id: u64,
         pub user: user::User,
-        pub order_id: Uuid,
+        pub trade_type: TradeType,
         pub crypto: crypto::Crypto,
         pub quantity: Decimal,
         pub price: Decimal,
@@ -113,13 +126,19 @@ pub mod order {
     pub struct Order {
         pub id: u64,
         pub user: user::User,
-        pub account_id: Uuid,
+        pub account: account::Account,
         pub order_type: OrderType,
         pub trade_action: TradeAction,
-        pub crypto_id: Uuid,
+        pub crypto: crypto::Crypto,
         pub quantity: Decimal,
         pub unfilled: Decimal,
         pub price: Decimal,
         pub created_at: u64,
+    }
+
+    impl Order {
+        pub fn update_unfilled(&mut self, executed_quantity: Decimal) {
+            self.unfilled -= executed_quantity;
+        }
     }
 }
--- src/tests/models.rs
+++ src/tests/models.rs
@@ -1,131 +1,147 @@
-// src/tests/models.rs
+use crate::domain::models::account;
+use crate::domain::models::crypto;
+use crate::domain::models::order;
+use crate::domain::models::position;
+use crate::domain::models::trade;
+use crate::domain::models::user;
+use rust_decimal_macros::dec;
+use uuid::Uuid;
 
-use crate::domain::models::account::Account;
-use crate::domain::models::crypto::Crypto;
-use crate::domain::models::portfolio::{Holding, Portfolio};
-use crate::domain::models::trade::{Trade, TradeType};
-use crate::domain::models::user::User;
+fn get_test_user() -> user::User {
+    user::User {
+        id: Uuid::new_v4(),
+        nickname: "John Doe".to_string(),
+        email: "john.doe@example.com".to_string(),
+    }
+}
 
-#[test]
-fn test_execute_trade_sufficient_funds() {
-    let mut account = Account {
+fn get_test_crypto() -> crypto::Crypto {
+    crypto::Crypto {
         id: 1,
-        user_id: 1,
-        cash_balance: 5000.0,
-    };
-
-    let trade = Trade {
-        id: 1,
-        user: User {
-            id: 1,
-            name: "testuser".to_string(),
-            email: "test@example.com".to_string(),
-            password: "password".to_string(),
-            created_at: "2023-04-09".to_string(),
-        },
-        trade_type: TradeType::Buy,
-        crypto: Crypto {
-            id: 1,
-            name: "Bitcoin".to_string(),
-            symbol: "BTC".to_string(),
-            price_usd: 1000.0,
-        },
-        quantity: 2.0,
-        price: 1000.0,
-        executed: false,
-        execution_date: None,
-    };
-
-    assert!(account.execute_trade(&trade).is_ok());
-    assert_eq!(account.cash_balance, 3000.0);
+        name: "Bitcoin".to_string(),
+        symbol: "BTC".to_string(),
+    }
 }
 
 #[test]
-fn test_execute_trade_insufficient_funds() {
-    let mut account = Account {
-        id: 1,
-        user_id: 1,
-        cash_balance: 1000.0,
+fn account_has_sufficient_cash() {
+    let account = account::Account {
+        id: Uuid::new_v4(),
+        user: get_test_user(),
+        cash: dec!(100.00),
     };
 
-    let trade = Trade {
+    assert!(account.has_sufficient_cash(dec!(50.00)));
+    assert!(!account.has_sufficient_cash(dec!(200.00)));
+}
+
+#[test]
+fn account_reserve_cash() {
+    let mut account = account::Account {
+        id: Uuid::new_v4(),
+        user: get_test_user(),
+        cash: dec!(100.00),
+    };
+
+    account.reserve_cash(dec!(25.00));
+    assert_eq!(account.cash, dec!(75.00));
+}
+
+#[test]
+fn position_can_sell() {
+    let position = position::Position {
+        account_id: Uuid::new_v4(),
+        crypto: get_test_crypto(),
+        quantity: dec!(10.00),
+        sellable_quantity: dec!(8.00),
+        buy_price: dec!(5000.00),
+        purchase: dec!(50000.00),
+        created_at: 0,
+    };
+
+    assert!(position.can_sell(dec!(5.00)));
+    assert!(!position.can_sell(dec!(10.00)));
+}
+
+#[test]
+fn position_reserve_for_sell() {
+    let mut position = position::Position {
+        account_id: Uuid::new_v4(),
+        crypto: get_test_crypto(),
+        quantity: dec!(10.00),
+        sellable_quantity: dec!(8.00),
+        buy_price: dec!(5000.00),
+        purchase: dec!(50000.00),
+        created_at: 0,
+    };
+
+    position.reserve_for_sell(dec!(2.00));
+    assert_eq!(position.sellable_quantity, dec!(6.00));
+}
+
+#[test]
+fn position_update_position() {
+    let mut position = position::Position {
+        account_id: Uuid::new_v4(),
+        crypto: get_test_crypto(),
+        quantity: dec!(10.00),
+        sellable_quantity: dec!(8.00),
+        buy_price: dec!(5000.00),
+        purchase: dec!(50000.00),
+        created_at: 0,
+    };
+
+    let trade = trade::Trade {
         id: 1,
-        user: User {
-            id: 1,
-            name: "testuser".to_string(),
-            email: "test@example.com".to_string(),
-            password: "password".to_string(),
-            created_at: "2023-04-09".to_string(),
+        user: get_test_user(),
+        trade_type: trade::TradeType::Buy,
+        crypto: get_test_crypto(),
+        quantity: dec!(5.00),
+        price: dec!(6000.00),
+        executed: true,
+        executed_at: 100,
+    };
+
+    position.update_position(&trade);
+    assert_eq!(position.quantity, dec!(15.00));
+    assert_eq!(position.sellable_quantity, dec!(13.00));
+    assert_eq!(position.buy_price, dec!(5333.3333333333333333333333333));
+}
+#[test]
+fn trade_total_cost() {
+    let trade = trade::Trade {
+        id: 1,
+        user: get_test_user(),
+        trade_type: trade::TradeType::Buy,
+        crypto: get_test_crypto(),
+        quantity: dec!(5.00),
+        price: dec!(6000.00),
+        executed: true,
+        executed_at: 100,
+    };
+
+    assert_eq!(trade.total_cost(), dec!(30000.00));
+}
+
+#[test]
+fn order_update_unfilled() {
+    let mut order = order::Order {
+        id: 1,
+        user: get_test_user(),
+        account: account::Account {
+            id: Uuid::new_v4(),
+            user: get_test_user(),
+            cash: dec!(100.00),
         },
-        trade_type: TradeType::Buy,
-        crypto: Crypto {
-            id: 1,
-            name: "Bitcoin".to_string(),
-            symbol: "BTC".to_string(),
-            price_usd: 1000.0,
-        },
-        quantity: 2.0,
-        price: 1000.0,
-        executed: false,
-        execution_date: None,
+        order_type: order::OrderType::Market,
+        trade_action: order::TradeAction::Buy,
+        crypto: get_test_crypto(),
+        quantity: dec!(10.00),
+        unfilled: dec!(10.00),
+        price: dec!(6000.00),
+        created_at: 0,
     };
 
-    assert_eq!(
-        account.execute_trade(&trade).unwrap_err(),
-        "Insufficient funds"
-    );
-}
-#[test]
-fn test_add_holding() {
-    let mut portfolio = Portfolio {
-        id: 1,
-        user_id: 1,
-        holdings: vec![],
-    };
-
-    portfolio.add_holding(1, 2.0);
-
-    assert_eq!(
-        portfolio.holdings,
-        vec![Holding {
-            crypto_id: 1,
-            quantity: 2.0,
-        }]
-    );
-}
-
-#[test]
-fn test_update_holding() {
-    let mut portfolio = Portfolio {
-        id: 1,
-        user_id: 1,
-        holdings: vec![Holding {
-            crypto_id: 1,
-            quantity: 2.0,
-        }],
-    };
-
-    assert!(portfolio.update_holding(1, 3.0).is_ok());
-    assert_eq!(
-        portfolio.holdings,
-        vec![Holding {
-            crypto_id: 1,
-            quantity: 3.0,
-        }]
-    );
-}
-
-#[test]
-fn test_remove_holding() {
-    let mut portfolio = Portfolio {
-        id: 1,
-        user_id: 1,
-        holdings: vec![Holding {
-            crypto_id: 1,
-            quantity: 2.0,
-        }],
-    };
-
-    assert!(portfolio.remove_holding(1).is_ok());
-    assert!(portfolio.holdings.is_empty());
+    order.update_unfilled(dec!(4.00));
+    assert_eq!(order.unfilled, dec!(6.00));
 }
